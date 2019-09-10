064301702b9e2431070e9ad0ae69dfe07cef54ad
Juan Melendres <juan.mele97@gmail.com>
2019-09-09 19:59:09 -0500

    Changes on main.go
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

--- labs/c_basics/fahrenheit_celcius.c
+++ labs/c_basics/fahrenheit_celcius.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define   LOWER  0       /* lower limit of table */
 #define   UPPER  300     /* upper limit */
 #define   STEP   20      /* step size */
 
 /* print Fahrenheit-Celsius table */
+int convert(int n)
+{
+  printf("Fahrenheit: %3d, Celcius: %6.1f\n", n, (5.0/9.0)*(n-32));
+}
+
+int convertRange(int start, int end, int increment)
+{
+  int n = start;
+  while (n <= end) {
+    printf("Fahrenheit: %3d, Celcius: %6.1f\n", n, (5.0/9.0)*(n-32));
+    n = n + increment;
+  }
+}
 
 int main(int argc, char **argv)
 {
-    int fahr;
+    int fahr, start, end, increment;
     if (argc < 2)
     {
       printf("You need to send the number of grades to convert\n");
-      printf("How to execute: ./fc <number>\n");
+      printf("How to execute option one: ./fc <number>\n");
       return 1;
     }
     fahr = atoi(argv[1]); //atoi converts a string into a integer
-
-    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
+    convert(fahr);
+    if (argc < 4)
+    {
+      printf("How to execute option two: ./fc <start> <end> <increment>\n");
+      return 1;
+    }
+    start = atoi(argv[1]);
+    end = atoi(argv[2]);
+    increment = atoi(argv[3]);
+    convertRange(start, end, increment);
     return 0;
 }
