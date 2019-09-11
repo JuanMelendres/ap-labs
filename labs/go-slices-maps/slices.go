99e5c6c55bec436df0dd22b75095d63d43bf1de0
Juan Melendres <juan.mele97@gmail.com>
2019-09-10 20:25:49 -0500

    Add the implementation of Pic function
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+  data := make([][]uint8, dy)
+  for i := range data {
+      data[i] = make([]uint8, dx)
+  }
+  for x := range data {
+      for y := range data[x] {
+          data[x][y] = uint8(x*y)
+      }
+  }
+  return data
+}
+
 func main() {
+  pic.Show(Pic)
 }
