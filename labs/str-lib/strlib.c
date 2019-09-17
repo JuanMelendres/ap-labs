c6f7b5325626b12e58f8bed6dff80e978f7a5e3a
Juan Melendres <juan.mele97@gmail.com>
2019-09-17 18:24:02 -0500

    Add strlib & main
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str) {
+  int lenght = 0;
+  int i;
+  for (i = 0; str[i] != 0; i++) {
+    lenght++;
+  }
+  return lenght;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition) {
+  int originLen = mystrlen(origin);
+  int addLen = mystrlen(addition);
+  char *charAdd = malloc(sizeof(char)*originLen + addLen);
+  strcpy(charAdd, origin);
+  strcpy(charAdd + originLen, addition);
+  return charAdd;
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int mystrfind(char *origin, char *substr) {
+  int i;
+  int aux = 0;
+  int originLen = mystrlen(origin);
+  int subLen = mystrlen(substr);
+  for (i = 0; i <= originLen; i++) {
+    if (origin[i] == substr[0] && aux < 1) {
+      aux++;
+    }
+    else if (origin[i] == substr[aux]) {
+      aux++;
+    }
+    else {
+      aux = 0;
+    }
+    if (aux >= subLen) {
+      return 1;
+    }
+  }
+  return 0;
 }
