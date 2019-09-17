3b0250941c749e08188b0a1cf3f1c1fddc5bcd69
Juan Melendres <juan.mele97@gmail.com>
2019-09-17 18:24:02 -0500

    Add strlib & main
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+  if (argc < 4) {
+    printf("Need to pass tree arguments\n");
+    printf("How to execute option one: ./main <string> <string2> <subString>\n");
+    return 1;
+  }
+  int lenght = mystrlen(argv[1]);
+  char *stringAdd = mystradd(argv[1], argv[2]);
+  int find = mystrfind(stringAdd, argv[3]);
+  if (find = 1) {
+    printf("Initial Lenght\t    : %d\nNew String\t    : %s\nSubString was found : yes\n", lenght, stringAdd);
+  }
+  else {
+    printf("Initial Lenght\t    : %d\nNew String\t    : %s\nSubString was found : no\n", lenght, stringAdd);
+  }
+  return 0;
 }
