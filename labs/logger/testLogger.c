552d0762ebd01eedb9a3cd57ee29a4547d768699
Juan Melendres <juan.mele97@gmail.com>
2019-10-03 21:59:47 -0500

    Add test
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
 
 int main() {
-    return 0;
+  infof("This is information test\n");
+	warnf("This is a %s test\n", "warn");
+	errorf("This is an %s %s\n", "error", "test");
+	panicf("This is a panic test\n");
+  return 0;
 }
